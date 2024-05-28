3bc94d9ff51500fe0278c08ce7ef72c7cbfe6ff0
Mike Gerasimenko <mihail@gerasimenko.me>
2024-05-28 23:03:46 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "XcodeSelectiveTesting",
+    platforms: [
+        .macOS(.v12),
+    ],
+    products: [
+        .executable(
+            name: "xcode-selective-test",
+            targets: ["xcode-selective-test"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "xcode-selective-test",
+            dependencies: ["SelectiveTestingCore",
+                        .product(name: "ArgumentParser", package: "swift-argument-parser")],
+            swiftSettings: sharedSettings
+        ),
+        .binaryTarget(
+            name: "xcode-selective-test",
+            url: "https://github.com/mikeger/XcodeSelectiveTesting/releases/download/0.9.4/xcode-selective-test.artifactbundle.zip",
+            checksum: "5814465535721294bb46a488d63fa0f2eb989428495bfb4c7b294b0b0eae53af"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,13 @@
+# XcodeSelectiveTesting Bianry
+
+This repository provides a binary precompiled version of [XcodeSelectiveTesting](https://github.com/mikeger/XcodeSelectiveTesting) tool.
+
+Please use this version in case you don't want to recompile the tool and trust repository author to provide a legitimate binary version.
+
+# How to use it
+
+Add to your Package.swift:
+
+```swift
+    .package(url: "git@github.com:mikeger/XcodeSelectiveTesting-Binary", .upToNextMajor(from: "0.9.4"))
+```
